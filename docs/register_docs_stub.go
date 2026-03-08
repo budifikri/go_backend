1485bb684fac1749b007a5bd858c75ddf02c8a23
budifikri <fikriawan2007@gmail.com>
2026-03-09 00:57:07 +0700

    update purcahse

---
+++ docs/register_docs_stub.go
@@ -0,0 +1,9 @@
+package docs
+
+import "github.com/gofiber/fiber/v2"
+
+// Register API docs routes.
+// This placeholder keeps the server runnable when Swagger isn't configured.
+func Register(app *fiber.App) {
+	// No-op
+}
