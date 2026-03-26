b1b5a2881eec3722f73d430ae273e17cce7bfb68
budifikri <fikriawan2007@gmail.com>
2026-03-26 10:11:43 +0700

    start.bat

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ========================================
+echo    Starting Go Server...
+echo ========================================
+
+go run cmd/server/main.go
+
+echo.
+echo Server stopped.
+pause
